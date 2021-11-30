1dc2302f2b2f30aca33ac953d17c4c5633921c59
Madhura-Kamat <53913024+Madhura-Kamat@users.noreply.github.com>
2021-11-30 10:42:16 +0530

    Add files via upload

---
+++ fileupload.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart">
+	<input type="file" name="file">
+	<button type="submit" name="sumbit"> Upload </button>
+
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,41 @@
+<?php
+if(isset($_POST['submit'])){
+	$file = $_FILES['file'];
+	//print($_file);
+	$fileName = $_FILES['file']['name'];
+	$fileTmpName = $_FILES['file']['tmp_name'];
+	$fileSize = $_FILES['file']['size'];
+	
+	$fileError = $_FILES['file']['error'];
+	
+	$fileType = $_FILES['file']['type'];
+	$fileExt = explode('.', $fileName); //to get file name with extension
+	$fileActualExt = strtolower(end($fileExt));
+	$allowed = array('jpg','jpeg','png','pdf');
+
+	if(in_array($fileActualExt, $allowed))
+	{
+		if(fileError === 0){
+			if($fileSize < 1000000 )
+			{
+				$fileNameNew = uniqid('',true).".".$fileActualExt;
+				$fileDestination = 'uploads/'.$fileNameNew;
+				move_uploaded_file($fileTmpName, $fileDestination);
+				header("Location:index.php?uploadsuccess");
+
+			} else{
+				echo "Your file is too big!";
+
+			}
+	}
+	else{
+		echo "There was an error uploading files";
+	}
+	else{
+		echo "You cannot upload files of these type";
+	}
+	
+
+}
+
+?>
